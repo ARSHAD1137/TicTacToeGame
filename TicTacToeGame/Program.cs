e7245c36e16b192ca8e610b93e9e2694d1c23dfe
Arshad <arshadpinjari666@gmail.com>
2021-05-17 13:31:10 +0530

    Added.TakingInput

--- TicTacToeGame/Program.cs
+++ TicTacToeGame/Program.cs
@@ -4,11 +4,30 @@ namespace TicTacToeGame
 {
     class Program
     {
-       
+        public static void GameDisplay()
+        {
+            Console.WriteLine("Select choice \n X \n O ");
+            string choice = Console.ReadLine();
+            switch(choice)
+            {
+                case "X":
+                    Console.WriteLine("You choice X");
+                    break;
+                case "O":
+                    Console.WriteLine("You choice O");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Choice");
+                    break;
+            }
+
+        }
         static void Main(string[] args)
         {
-
+           
             Console.WriteLine("Welcome To Tic Tac Toe Game");
+            Console.WriteLine("Player 1 : X");
+            Console.WriteLine("Player 2 : O "); 
             
         }
     }
