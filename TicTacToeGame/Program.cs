676d2a96e624804547f3f9ee323545bba1028e26
Arshad <arshadpinjari666@gmail.com>
2021-05-19 18:20:58 +0530

    Added.TakingUserInput

--- TicTacToeGame/Program.cs
+++ TicTacToeGame/Program.cs
@@ -12,11 +12,33 @@ namespace TicTacToeGame
             {
                 board[i] = ' ';
             }
+           
+        }
+        static void SelectLetter()
+        {
+            Console.WriteLine("Select The Letter Between 'X or 'O");
+            string userLetter = Console.ReadLine();
+           
+            if (userLetter.Equals("X") || userLetter.Equals("x"))
+            {
+                Console.WriteLine("You Choose 'X'");
+                string computerLetter = "O";
+            }
+            else if (userLetter.Equals("O") || userLetter.Equals("o"))
+            {
+                Console.WriteLine("You Choose 'O'");
+                string computerLetter = "X";
+            }
+            else
+            {
+                Console.WriteLine("Invalid Letter");
+            }
         }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Tic Tac Toe Game");
             BoardDisplay();
+            SelectLetter();
         }
     }
         
