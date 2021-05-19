a3592efc5485693535202f4126af15359ef123ba
Arshad <arshadpinjari666@gmail.com>
2021-05-19 18:23:35 +0530

    Added.board

--- TicTacToeGame/Program.cs
+++ TicTacToeGame/Program.cs
@@ -12,7 +12,25 @@ namespace TicTacToeGame
             {
                 board[i] = ' ';
             }
-           
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", board[1], board[2], board[3]);
+
+            Console.WriteLine("_____|_____|_____ ");
+
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", board[4], board[5], board[6]);
+
+            Console.WriteLine("_____|_____|_____ ");
+
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", board[7], board[8], board[9]);
+
+            Console.WriteLine("     |     |      ");
+
+
         }
         static void SelectLetter()
         {
